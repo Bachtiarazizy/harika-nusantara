181da5f5540a4a3233ba329be58f7fa08e46b713
Bachtiarazizy <bachtiarazizy@gmail.com>
2025-08-13 16:24:08 +0300

    image resolution

--- components/home/ValueSection.jsx
+++ components/home/ValueSection.jsx
@@ -10,9 +10,7 @@ export default function ValueSection() {
   return (
     <section className="w-full flex flex-col lg:flex-row overflow-hidden">
       {/* gambar */}
-      <motion.div className="flex justify-center hidden md:block lg:justify-end lg:w-1/2" initial={{ opacity: 0, x: -60 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true, amount: 0.3 }}>
-        <Image src="/images/farmer.jpg" alt="Indonesian Coffee Farmers Working Together" width={100} height={100} className="w-full h-full object-cover" />
-      </motion.div>
+      <Image src="/images/farmer.jpg" alt="Indonesian Coffee Farmers Working Together" width={1200} height={800} quality={100} className="w-full h-full object-cover" />
 
       {/* text content */}
       <motion.div className="lg:w-1/2 px-6 md:px-12 py-12 flex items-center bg-[#483B36]" initial={{ opacity: 0, x: 60 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.8, delay: 0.2 }} viewport={{ once: true, amount: 0.3 }}>
